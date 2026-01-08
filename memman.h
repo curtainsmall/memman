51781d01a3699ee2d10ceadf7b5dccf5702d66d0
curtainsmall <curtainsmall@126.com>
2026-01-08 14:49:05 +0800

    Added max and min macro; Added assert for count; Added flags for m3_memman; Fixed buffer manupulators;

--- memman.h
+++ memman.h
@@ -7,23 +7,22 @@
 #define MEMMAN_EXTERN
 #endif // !MEMMAN_EXTERN
 
-
-#include <memory.h>
 #include <stdint.h>
-#include <string.h>
 
-#define m3_memset(dst, val, size) ((void) memset(dat, val, size))
-#define m3_memcpy(dst, src, size) ((void) memcpy(dst, src, size))
-#define m3_memmove(dst, src, size) ((void) memmove(dst, src, size))
-#define m3_strcpy(dst, src) ((void) strcpy(dst, src))
+typedef enum m3_flag
+{
+    M3_NONE = 0,
+    M3_NO_PANIC = 1 << 0,
+    M3_NO_ZERO_INIT = 1 << 1,
+} m3_flag_t;
 
-MEMMAN_EXTERN void* m3_memman(void* ptr, size_t new_size);
+MEMMAN_EXTERN void* m3_memman(void* ptr, size_t new_size, m3_flag_t flag);
 MEMMAN_EXTERN size_t m3_memsize(void* ptr);
 #define m3_strsize(str) (m3_memsize(str) - 1)
 
-#define m3_alloc(size) m3_memman(NULL, size)
-#define m3_realloc(ptr, size) m3_memman(ptr, size)
-#define m3_free(ptr) ((void) m3_memman(ptr, 0))
+#define m3_alloc(size) m3_memman(NULL, size, M3_NONE)
+#define m3_realloc(ptr, size) m3_memman(ptr, size, M3_NONE)
+#define m3_free(ptr) ((void) m3_memman(ptr, 0, M3_NONE))
 
 #define m3_memnav(ptr, Type, offset) ((Type*)(ptr) + (offset))
 
@@ -50,8 +49,18 @@ MEMMAN_EXTERN const char* m3_make_str_v(const char* fmt, va_list va);
 #include <assert.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <memory.h>
+#include <string.h>
 
-void* m3_memman(void* ptr, size_t new_size)
+#ifndef max
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#endif // !max
+
+#ifndef min
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#endif // !min
+
+void* m3_memman(void* ptr, size_t new_size, m3_flag_t flag)
 {
     size_t old_size = m3_memsize(ptr);
 
@@ -72,10 +81,18 @@ void* m3_memman(void* ptr, size_t new_size)
         void* tmp = realloc(mem, new_size + sizeof(size_t));
         if(!tmp)
         {
-            printf(stderr, "Out of Memory");
+            if(flag & M3_NO_PANIC)
+                return NULL;
+
+            (void) printf(stderr, "Out of Memory");
             abort();
         }
+
         *m3_memnav(tmp, size_t, 0) = new_size;
+
+        if(!(flag & M3_NO_ZERO_INIT) && new_size > old_size)
+            (void) memset(m3_memnav(m3_memnav(tmp, size_t, 1),uint8_t, old_size), 0, new_size - old_size);
+
         return m3_memnav(tmp, size_t, 1);
     }
 }
@@ -95,16 +112,11 @@ static intmax_t rel2abs(intmax_t idx, uintmax_t count)
     return (idx %= count) < 0 ? idx + count : idx;
 }
 
-struct buf
-{
-    size_t elemsize;
-    uintmax_t count;
-    uint8_t values[];
-};
-#define bufelemsize(ptr) (*m3_memnav(ptr, size_t, -2))
-#define bufsize(ptr) (*m3_memnav(ptr, size_t, -1))
-#define bufhead(ptr) m3_memnav(ptr, size_t, -2)
-#define bufinfosize_v (2 * sizeof(size_t))
+#define bufinfosize_v sizeof(size_t)
+#define bufsize(buf) (*m3_memnav(buf, size_t, -2))
+#define bufvaluesize(buf) (bufsize(buf) - bufinfosize_v)
+#define bufelemsize(buf) (*m3_memnav(buf, size_t, -1))
+#define bufvaluecount(buf) (bufvaluesize(buf) / bufelemsize(buf))
 
 void m3_buf_init(void** p_buf, size_t elemsize)
 {
@@ -113,10 +125,9 @@ void m3_buf_init(void** p_buf, size_t elemsize)
     if(*p_buf)
         return;
 
-    struct buf* buf = m3_alloc(sizeof(*buf));
-    buf->elemsize = elemsize;
-    buf->count = 0;
-    *p_buf = buf->values;
+    size_t* ptr = m3_alloc(bufinfosize_v);
+    *ptr = elemsize;
+    *p_buf = ptr + 1;
 }
 
 void m3_buf_drop(void** p_buf)
@@ -125,7 +136,7 @@ void m3_buf_drop(void** p_buf)
 
     if(!*p_buf)
         return;
-    m3_free(bufhead(*p_buf));
+    m3_free(m3_memnav(*p_buf, size_t, -1));
     *p_buf = NULL;
 }
 
@@ -135,45 +146,58 @@ void m3_buf_clear(void** p_buf)
 
     if(!*p_buf)
         return;
-    *p_buf = m3_realloc(bufhead(*p_buf), bufinfosize_v);
+    *p_buf = m3_realloc(m3_memnav(*p_buf, size_t, -1), bufinfosize_v);
 }
 
-size_t m3_buf_size(void* ptr)
+size_t m3_buf_size(void* buf)
 {
-    return bufsize(ptr);
+    return bufsize(buf);
 }
 
-uintmax_t m3_buf_count(void* ptr)
+uintmax_t m3_buf_count(void* buf)
 {
-    return bufsize(ptr) / bufelemsize(ptr);
+    return bufvaluecount(buf);
 }
 
 void m3_buf_insert_n(void** p_buf, intmax_t idx, void* val, uintmax_t count)
 {
     assert(p_buf);
     assert(val);
-    assert(count > 0);
+    assert(count <= INTMAX_MAX);
 
-    idx = rel2abs(idx, count);
+    if(count == 0)
+        return;
 
-    struct buf* buf = bufhead(*p_buf);
+    uint8_t* buf = *p_buf;
 
-    size_t value_size = m3_memsize(buf) - bufinfosize_v;
-    size_t value_size_wanted = (buf->count + count) * buf->elemsize;
+    size_t value_size = bufvaluesize(buf);
 
+    size_t value_size_wanted = (bufvaluecount(buf) + count) * bufelemsize(buf);
+    size_t offset = idx * bufelemsize(buf);
+    size_t inserted_size = bufelemsize(buf) * count;
+
+#if 0
+    if(idx > bufvaluecount(buf))
+    {
+        value_size_wanted += (idx - bufvaluecount(buf)) * bufelemsize(buf);
+    }
+    else if(idx < 0)
+    {
+        value_size_wanted += -idx * bufelemsize(buf);
+        offset = 0;
+    }
+#endif
+    
     if(value_size_wanted >= value_size)
     {
         value_size = max(value_size_wanted, value_size * 2);
-        buf = m3_realloc(buf, value_size + bufinfosize_v);
-        *p_buf = buf->values;
+        void* new_head = m3_realloc(m3_memnav(buf, size_t, -1), value_size + bufinfosize_v);
+        *p_buf = buf = m3_memnav(new_head, size_t, 1);
     }
 
-    size_t offset = idx * buf->elemsize;
-    size_t inserted_size = buf->elemsize * count;
-    uint8_t* target_pos = buf->values + offset;
-    m3_memmove(target_pos + inserted_size, target_pos, value_size - offset - inserted_size);
-    m3_memcpy(target_pos, val, inserted_size);
-    buf->count += count;
+    uint8_t* target_pos = buf + offset;
+    (void) memmove(target_pos + inserted_size, target_pos, value_size - offset - inserted_size);
+    (void) memcpy(target_pos, val, inserted_size);
 }
 
 void m3_buf_insert(void** p_buf, intmax_t idx, void* val, uintmax_t count)
@@ -194,25 +218,29 @@ void m3_buf_insert_back(void** p_buf, void* val)
 void m3_buf_erase_n(void** p_buf, intmax_t idx, uintmax_t count)
 {
     assert(p_buf);
-    assert(count > 0);
+    assert(count <= INTMAX_MAX);
+
+    if(count == 0)
+        return;
+
+    uint8_t* buf = *p_buf;
 
     idx = rel2abs(idx, count);
 
-    struct buf* buf = bufhead(*p_buf);
+    size_t value_size = bufvaluesize(buf);
+    size_t offset = idx * bufelemsize(buf);
+    size_t erased_size = bufelemsize(buf) * count;
+    uint8_t* target_pos = buf + offset;
+    size_t moved_size = value_size - offset - erased_size;
+    (void) memmove(target_pos, target_pos + erased_size, moved_size);
+    (void) memset(target_pos + moved_size, 0, erased_size);
 
-    size_t value_size = m3_memsize(buf) - bufinfosize_v;
-    size_t offset = idx * buf->elemsize;
-    size_t erased_size = buf->elemsize * count;
-    uint8_t* target_pos = buf->values + offset;
-    m3_memmove(target_pos, target_pos + erased_size, value_size - offset - erased_size);
-    buf->count -= count;
-
-    size_t value_size_wanted = buf->count + count;
-    if(value_size_wanted * buf->elemsize * 2 < value_size)
+    size_t value_size_wanted = bufvaluecount(buf) - count;
+    if(value_size_wanted * bufelemsize(buf) * 2 < value_size)
     {
         value_size = min(value_size_wanted, value_size / 2);
-        buf = m3_realloc(buf, value_size + bufinfosize_v);
-        *p_buf = buf->values;
+        void* new_head = m3_realloc(m3_memnav(buf, size_t, -1), value_size + bufinfosize_v);
+        *p_buf = m3_memnav(new_head, size_t, 1);
     }
 }
 
@@ -223,7 +251,7 @@ void m3_buf_erase(void** p_buf, intmax_t idx, uintmax_t count)
 
 void m3_buf_erase_back_n(void** p_buf, uintmax_t count)
 {
-    m3_buf_erase_n(p_buf, m3_buf_count(*p_buf) - 1, count);
+    m3_buf_erase_n(p_buf, m3_buf_count(*p_buf) - count, count);
 }
 
 void m3_buf_erase_back(void** p_buf)
--- tests/main.c
+++ tests/main.c
@@ -11,12 +11,22 @@ struct test_struct
 
 int main()
 {
+    struct test_struct tss[] = {
+        {.i = 0, .f = 42},
+        {.i = 1, .f = 42}
+    };
+
     m3_buf_tt(struct test_struct) ts = { 0 };
     m3_buf_init(&ts, sizeof(struct test_struct));
 
-    m3_buf_insert_back(&ts, &(struct test_struct) { .i = 42, .f = 42.8 });
+    m3_buf_insert_back(&ts, tss);
+    printf("Struct: %d, %f\n", ts[0].i, ts[0].f);
+    m3_buf_erase_back(&ts);
 
-    printf("Struct: %d, %f", ts[0].i, ts[0].f);
+    m3_buf_insert_back_n(&ts, tss, 2);
+    printf("Struct: %d, %f\n", ts[0].i, ts[0].f);
+    printf("Struct: %d, %f\n", ts[1].i, ts[1].f);
+    m3_buf_erase_back_n(&ts, 2);
 
     m3_buf_drop(&ts);
 
